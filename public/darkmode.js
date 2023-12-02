25cea8fe0730866398b9890b5ddc3149fb311d65
yueunkim <yueunkim0605@gmail.com>
2023-12-02 10:31:38 +0900

    Feat: html 마크업, css 스타일링, 다크모드 기눙 추가, 다음/이전 뉴스 버튼 추가

---
+++ public/darkmode.js
@@ -0,0 +1,24 @@
+const darkmodeBtn = document.getElementById('darkmode-btn');
+const summaryBtn = document.getElementById('summary-button');
+const html = document.documentElement;
+const divs = document.querySelectorAll('div');
+const logo = document.getElementById('logo');
+const icon = darkmodeBtn.querySelector('i');
+
+darkmodeBtn.addEventListener('click', () => {
+  html.classList.toggle('dark-mode');
+  document.body.classList.toggle('dark-mode');
+  summaryBtn.classList.toggle('dark-mode');
+  divs.forEach((div) => div.classList.toggle('dark-mode'));
+  // 다크모드일 때
+  if (document.body.classList.contains('dark-mode')) {
+    logo.src = './images/logo_White.png';
+    icon.className = 'fa-solid fa-sun';
+    icon.style.color = '#ffffff';
+  // 아닐 때
+  } else {
+    logo.src = './images/logo_Green.png';
+    icon.className = 'fa-solid fa-moon';
+    icon.style.color = '#000000';
+  }
+});
